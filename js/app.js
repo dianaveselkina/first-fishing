6dac95cd2e5a403375b950696dba57ff0dfa63d4
dianaveselkin <vess_di@mail.ru>
2023-04-18 00:25:46 +0300

    timer

---
+++ js/app.js
@@ -0,0 +1,44 @@
+const start = document.querySelector('#start');
+let text = document.getElementById('text');
+let timerShow = document.getElementById('timer');
+const fishs = document.querySelectorAll('.fish');
+
+start.addEventListener('click', function () {
+  fishs.forEach((el) => {
+    el.classList.toggle('fish-active');
+  });
+
+  timeMinut = 11;
+  hideBtn();
+});
+
+timer = setInterval(function () {
+  seconds = timeMinut % 60;
+
+  if (timeMinut <= 0) {
+    timerShow.innerHTML = 'Увы... Ваше время истекло';
+    clearInterval(timer);
+  } else {
+    let strTimer = `${0}:${seconds}`;
+    timerShow.innerHTML = strTimer;
+  }
+  --timeMinut;
+}, 1000);
+
+function hideBtn() {
+  start.hidden = true;
+}
+
+for (let fish of fishs) {
+  fish.addEventListener('click', function () {
+    this.classList.add('displayd');
+  });
+}
+
+for (let fish of fishs) {
+  count = 0;
+  fish.onclick = function () {
+    count += 1;
+    text.innerHTML = 'Ваш улов равен ' + count + ' шт.';
+  };
+}
